bf30ec0fcaf0085a6dc5603073e31f46da781139
Waqar Ahmad Hanifi <kreazzyprogramer@gmail.com>
2021-07-03 21:44:39 +0500

    added month function

--- src/App.js
+++ src/App.js
@@ -28,20 +28,20 @@ function App() {
 
 
   const dateBuilder = (d) => {
-    // let months = [
-    //   "January",
-    //   "February",
-    //   "March",
-    //   "April",
-    //   "May",
-    //   "June",
-    //   "July",
-    //   "August",
-    //   "September",
-    //   "October",
-    //   "November",
-    //   "December",
-    // ];
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
     let days = [
       "Sunday",
       "Monday",
@@ -53,11 +53,11 @@ function App() {
     ];
     let day = days[d.getDay()];
     let date = d.getDate();
-    // let month = months[d.getMonths()];
+    let month = months[d.getMonth()];
 
     let year = d.getFullYear();
 
-    return `${day} ${date} ${year}`;
+    return `${day} ${date} ${month} ${year}`;
   };
   return (
     <div  className={(typeof weather.main != "undefined") ? ((weather.main.temp > 25) ? 'app warm' : 'app') : 'app'}>
